391a5fbe853d10cbad5565d8186238a6c3b22d8b
Andrinator <andrikhere@gmail.com>
2023-04-10 18:47:58 +0400

    Test

--- Integrations/GLPI/plugins/notify/setup.php
+++ Integrations/GLPI/plugins/notify/setup.php
@@ -9,22 +9,11 @@ function plugin_init_notify() {
    $PLUGIN_HOOKS['item_update']['notify'] = [
       'Computer' => 'plugin_item_update_notify'
    ];
-   $PLUGIN_HOOKS['item_update']['notify'] = [
-      'Computer_Software' => 'plugin_item_update_notify',
-      'Computer' => 'plugin_item_update_notify',
-      'Computer_SoftwareVersion' => 'plugin_item_update_notify',
-      'Software' => 'plugin_item_update_notify',
-      'Software_Computer' => 'plugin_item_update_notify',
-      'SoftwareVersion' => 'plugin_item_update_notify'
-   ];
+
    $PLUGIN_HOOKS['item_add']['notify'] = [
-      'Computer_Software' => 'plugin_item_add_notify',
-      'Computer' => 'plugin_item_add_notify',
-      'Computer_SoftwareVersion' => 'plugin_item_add_notify',
-      'Software' => 'plugin_item_add_notify',
-      'Software_Computer' => 'plugin_item_add_notify',
-      'SoftwareVersion' => 'plugin_item_add_notify'
+      'Computer' => 'plugin_item_add_notify'
    ];
+
    $PLUGIN_HOOKS['item_add']['notify'] = [
       'Ticket_User' => 'plugin_ticket_notify'
    ];
