f18ef7a180670b3c55fbf8a5abc134c6434ad1ef
Andrinator <andrikhere@gmail.com>
2023-04-19 08:29:45 +0400

    Test

--- Integrations/GLPI/plugins/restapiconnector/hook.php
+++ Integrations/GLPI/plugins/restapiconnector/hook.php
@@ -1,12 +1,10 @@
 <?php
+
 function plugin_restapiconnector_install() {
 
    ini_set("max_execution_time", "0");
 
    $migrationname = 'Migration';
-
-   require_once(PLUGIN_RESTAPICONNECTOR_DIR . "/install/update.php");
-   $version_detected = pluginRestapiconnectorGetCurrentVersion();
 
    require_once(PLUGIN_RESTAPICONNECTOR_DIR . "/install/install.php");
    pluginRestapiconnectorInstall(PLUGIN_RESTAPICONNECTOR_VERSION, $migrationname);
